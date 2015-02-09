b980223fb6970f93833f198687f2e2ff33e3fdae
Vasily Maleev <vasiliy.maleev@gmail.com>
2015-02-09 00:33:30 -0800

    All solution

---
+++ MessageBoard/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MessageBoard.Models;
+using MessageBoard.Services;
+
+namespace MessageBoard.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IMailService _mail;
+
+        public HomeController(IMailService mail)
+        {
+            _mail = mail;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactModel model)
+        {
+            var msg = string.Format("Comment From: {1}{0}Email: {2}{0}Website: {3}{0}Comment: {4}{0}",
+                Environment.NewLine, model.Name, model.Email, model.Website, model.Comment);
+
+            
+            if(_mail.SendMail("noreply@sadfsdfdsf.asdasd", "asdasd@dsffsdfsdfsdf", "Comment from website", msg))
+            {
+                ViewBag.MailSent = true;
+            }
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult MyMessages()
+        {
+            return View();
+        }
+
+        [Authorize(Roles="Admin")]
+        public ActionResult Moderation()
+        {
+            return View();
+        }
+    }
+}
